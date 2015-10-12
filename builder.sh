b64f58e3b7138ac20e9908952eb4f074f6cf5642
Matteo Nastasi <nastasi@openquake.org>
2015-10-12 10:49:32 +0000

    improved builder.sh

--- builder.sh
+++ builder.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+PKGNAME=h5py
+set -x
 set -e
 
 destdir="$(head -n 1 debian/changelog  | sed 's/\([^ ]*\)[ ]*(\([^-)]*\).*/\1-\2/g')"
     
 if [ "$1" = "build" ]; then
-    mkdir "$destdir"
-    git archive HEAD | tar -x -C "$destdir"
-    rm -rf "$destdir"/orig
+    shift
+    mkdir -p "build/$destdir"
+    git archive HEAD | tar -x -C "build/$destdir"
+    mv "build/$destdir"/orig/* "build/"
+    rm -rf "build/$destdir"/orig
+    rm -rf "build/$destdir"/builder.sh
+    cd "build/$destdir"
+    dpkg-buildpackage
 elif [ "$1" = "clean" ]; then
-    rm -rf "$destdir"
+    rm -rf "build"
 fi
+
